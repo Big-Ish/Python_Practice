c468d70e471bfbbb52961ac2323675a4862d094b
Big-Ish <ishkandamishra@yahoo.com>
2023-02-16 18:29:02 +0100

    testing commit

--- Functions/function_exam copy.py
+++ Functions/function_exam copy.py
@@ -5,6 +5,7 @@ def computepay(hours, payrate_base):
         grosspay = float(1.5*(hours-40.00)*(payrate_base)+40.00*payrate_base)
     return grosspay
 
+
 hours = int(input("Enter you work hours: "))
 payrate_base = float(input("Enter your payrate: "))
 
--- Functions/rock_paper_scissors.py
+++ Functions/rock_paper_scissors.py
@@ -1,7 +1,8 @@
 import random
 
-opt = [1,2,3]
+opt = [1, 2, 3]
 cpu = random.choice(opt)
+
 
 def cpuchoice():
     if cpu == 1:
@@ -11,6 +12,7 @@ def cpuchoice():
     elif cpu == 3:
         print("Computer chose: Scissors")
 
+
 def userchoice(usr_choice):
     if usr_choice == 1:
         print("User chose: Rock")
@@ -18,6 +20,7 @@ def userchoice(usr_choice):
         print("User chose: Paper")
     elif usr_choice == 3:
         print("User chose: Scissors")
+
 
 def winner(usr_choice):
     if cpu == usr_choice:
@@ -31,10 +34,12 @@ def winner(usr_choice):
     else:
         print("User wins!")
 
+
 def main():
     while True:
         try:
-            usr_choice = int(input("\nEnter '1' for Rock, '2' for Paper or '3' for Scissors: "))
+            usr_choice = int(
+                input("\nEnter '1' for Rock, '2' for Paper or '3' for Scissors: "))
             if usr_choice >= 4 or usr_choice <= 0:
                 print("You think you're smart, eh? Try again ;)")
                 continue
@@ -44,13 +49,16 @@ def main():
                 userchoice(usr_choice)
                 print("")
                 winner(usr_choice)
-                again = input(("\nWould you like to play again? ('y' for yes): "))
+                again = input(
+                    ("\nWould you like to play again? ('y' for yes): "))
                 if again.lower() == "y":
                     continue
                 else:
                     print("Program closed!")
                     break
-                
+
         except ValueError:
             print("Input must be an Integer!")
+
+
 main()
