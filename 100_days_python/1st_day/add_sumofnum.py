0a2cada80a60b8e4128f91e17d7b32fb6701647b
Big-Ish <ishkandamishra@yahoo.com>
2023-03-06 19:01:32 +0100

    update

---
+++ 100_days_python/1st_day/add_sumofnum.py
@@ -0,0 +1,6 @@
+a = input("Enter a two digit num: ")
+
+num1 = int(a[0])
+num2 = int(a[1])
+
+print(f'The sum of the digit', a, 'is', num1+num2)
---
+++ Codewars_practice/split_strings.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+
+
+def solutions(s):
+    str_list = []
+    for i in s:
+        str_list.append(i)
+
+    combined = []
+    for i in str_list:
+        for j in str_list:
+            pass
+        # continue here...
+
+    print(str_list)
+
+
+s = str(input("Enter a string: "))
+
+solutions(s)
