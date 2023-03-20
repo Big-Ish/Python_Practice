905e8c8755651532a9b9bfa23b7eb459b97b9c2c
Big-Ish <ishkandamishra@yahoo.com>
2023-03-21 00:23:26 +0100

    day 6

---
+++ 100_days_python/6th_day/hurdle.py
@@ -0,0 +1,52 @@
+# https://reeborg.ca/reeborg.html?
+# lang=en&mode=python&menu=worlds%2Fmenus%
+# 2Freeborg_intro_en.json&name=Hurdle%201&url=
+# worlds%2Ftutorial_en%2Fhurdle1.json
+
+# Hurdle 3
+# -------------------
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# def path():
+#     if wall_in_front():
+#         turn_left()
+#     elif front_is_clear():
+#         turn_right()
+
+
+# while at_goal() != True:
+#     path()
+#     if front_is_clear():
+#         move()
+
+
+# HURDLE 4 & MAZE
+# ------------------
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def pathfinder():
+#     if wall_in_front() and wall_on_right():
+#         turn_left()
+#     elif is_facing_north() and right_is_clear() and front_is_clear():
+#         turn_right()
+#         move()
+#     elif right_is_clear():
+#         turn_right()
+#         move()
+#     elif wall_in_front():
+#         turn_left()
+#     else:
+#         move()
+
+# while at_goal() != True:
+#     pathfinder()
+
+# -----------------
