8681e1b8277adfb114515a256dc378639d0e705f
Big-Ish <ishkandamishra@yahoo.com>
2023-04-03 00:26:17 +0200

    added rock paper scissors to 4th day

---
+++ 100_days_python/4th_day/rock_paper_scissors.py
@@ -0,0 +1,63 @@
+import random
+
+opt = [1, 2, 3]
+cpu = random.choice(opt)
+
+
+def cpuchoice():
+    if cpu == 1:
+        print("Computer chose: Rock")
+    elif cpu == 2:
+        print("Computer chose: Paper")
+    elif cpu == 3:
+        print("Computer chose: Scissors")
+
+
+def userchoice(usr_choice):
+    if usr_choice == 1:
+        print("User chose: Rock")
+    elif usr_choice == 2:
+        print("User chose: Paper")
+    elif usr_choice == 3:
+        print("User chose: Scissors")
+
+
+def winner(usr_choice):
+    if cpu == usr_choice:
+        print("Tie!")
+    elif cpu == 1 and usr_choice == 3:
+        print("Computer wins! Better luck next time ;)")
+    elif cpu == 2 and usr_choice == 1:
+        print("Computer wins! Better luck next time ;)")
+    elif cpu == 3 and usr_choice == 2:
+        print("Computer wins! Better luck next time ;)")
+    else:
+        print("User wins!")
+
+
+def main():
+    while True:
+        try:
+            usr_choice = int(
+                input("\nEnter '1' for Rock, '2' for Paper or '3' for Scissors: "))
+            if usr_choice >= 4 or usr_choice <= 0:
+                print("You think you're smart, eh? Try again ;)")
+                continue
+            else:
+                print("")
+                cpuchoice()
+                userchoice(usr_choice)
+                print("")
+                winner(usr_choice)
+                again = input(
+                    ("\nWould you like to play again? ('y' for yes): "))
+                if again.lower() == "y":
+                    continue
+                else:
+                    print("\nUser didn't input 'y'. Program closed!\n")
+                    break
+        except ValueError:
+            print("Input must be an Integer!")
+
+
+main()
